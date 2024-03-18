1c0733c0ef8081b85d2dfb51d2ba4b7723fd242f
Ben Holmes <benholmes.48@outlook.com>
2024-03-18 23:02:33 +1000

    Add poetry and ruff for dep managment and linting

--- src/static/__init__.py
+++ src/static/__init__.py
@@ -1,17 +1,4 @@
-import logging
-
-from src.models import BBombsBot
-
-try:
-    from .config import Config
-except ImportError:
-    logging.fatal("Config file not found aborting.")
-    exit(1)
-
-bot = BBombsBot(Config())
-
-if __name__ == "__main__":
-    bot.run()
+from .const import *
 
 
 # Copyright (C) 2024 BBombs
